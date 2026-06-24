2360d4d4f08319b0714458dc33783c487ec06acc
Aman11b <amansingh1100b@gmail.com>
2026-06-25 03:02:09 +0530

    hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import HeroCarousel from "./HeroCarousel";
 
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+          We are chaging the way people shop
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+          repellat labore iste similique impedit ad libero harum dicta dolor
+          autem consequuntur laboriosam, molestias, ipsum exercitationem. Nisi
+          ut dolorum corporis magni?
+        </p>
+        <Button asChild size="lg" className="mt-10">
+          <Link to="/products">Our Products</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 }
