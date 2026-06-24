82c9387db9066040ec14eadf2c240a7ea3971f1c
Aman11b <amansingh1100b@gmail.com>
2026-06-25 00:14:59 +0530

    About page  Component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,19 @@
 function About() {
-  return <h1 className="text-4xl">About Page</h1>;
+  return (
+    <section>
+      <h1 className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl">
+        We love{" "}
+        <span className="bg-primary py-2 px-4 rounded-lg tracking-widest text-white">
+          comfy
+        </span>
+      </h1>
+      <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima sint ea
+        quis officia eligendi vel dolor, voluptate sapiente quos corrupti, a
+        provident sed labore repudiandae eveniet nesciunt error voluptas
+        perspiciatis?
+      </p>
+    </section>
+  );
 }
 export default About;
