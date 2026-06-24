8867011c8e708b33041e9a69158b669745f3cc70
Aman11b <amansingh1100b@gmail.com>
2026-06-24 22:33:55 +0530

    NavList component

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,3 +1,23 @@
+import { NavLink } from "react-router-dom";
+import { links } from "../utils";
+import { Label } from "radix-ui";
+
 export default function NavLinks() {
-  return <div>NavLinks</div>;
+  return (
+    <div className="hidden lg:flex justify-center items-center gap-x-4">
+      {links.map((link) => {
+        return (
+          <NavLink
+            to={link.href}
+            key={link.label}
+            className={({ isActive }) => {
+              return `capitalize font-light tracking-wide ${isActive ? "text-primary" : ""}`;
+            }}
+          >
+            {link.label}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { CartButton, LinkDropdown, Logo, ModeToggle, NavLinks } from ".";
 export default function Navbar() {
   return (
     <nav className=" bg-muted py-4 flex flex-row">
-      <div className="align-element flex justify-between w-full">
+      <div className="align-element flex justify-between items-center w-full">
         <Logo />
         <LinkDropdown />
         <NavLinks />
