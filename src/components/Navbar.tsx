dd5ef1a652b6f17b8cdaba6fc58158796b0eefed
Aman11b <amansingh1100b@gmail.com>
2026-06-24 20:32:05 +0530

    Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { CartButton, LinkDropdown, Logo, ModeToggle, NavLinks } from ".";
+
+export default function Navbar() {
+  return (
+    <nav className=" bg-muted py-4 flex flex-row">
+      <div className="align-element flex justify-between w-full">
+        <Logo />
+        <LinkDropdown />
+        <NavLinks />
+      </div>
+      <div className=" align-element flex justify-center items-center gap-x-4">
+        <ModeToggle />
+        <CartButton />
+      </div>
+    </nav>
+  );
+}
