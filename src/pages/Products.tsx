0a252f8fcbb8a7f771c07b972a9c13952bc423a4
Aman11b <amansingh1100b@gmail.com>
2026-06-25 19:42:13 +0530

    default value

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,21 +1,25 @@
 import { Filters, ProductsContainer, PaginationContainer } from "../components";
-import { customFetch, type ProductsResponse } from "../utils";
+import {
+  customFetch,
+  type ProductsResponse,
+  type ProductsResponseWithParams,
+} from "../utils";
 import { type LoaderFunction } from "react-router-dom";
 
 const url = "/products";
 
 export const loader: LoaderFunction = async ({
   request,
-}): Promise<ProductsResponse> => {
+}): Promise<ProductsResponseWithParams> => {
   const params = Object.fromEntries([
     ...new URL(request.url).searchParams.entries(),
   ]);
-  console.log(params);
-  const response = await customFetch<ProductsResponse>(url, {
+  // console.log(params);
+  const response = await customFetch<ProductsResponseWithParams>(url, {
     params,
   });
 
-  return { ...response.data };
+  return { ...response.data, params };
 };
 function Products() {
   return (
