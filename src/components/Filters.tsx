a0db98d82bf56ead8933686330dd876c405404d1
Aman11b <amansingh1100b@gmail.com>
2026-06-25 21:22:00 +0530

    form select

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -2,10 +2,11 @@ import { Form, useLoaderData, Link } from "react-router-dom";
 import FormInput from "./FormInput";
 import { Button } from "./ui/button";
 import type { ProductsResponseWithParams } from "../utils";
+import FormSelect from "./FormSelect";
 
 export default function Filters() {
   const { meta, params } = useLoaderData() as ProductsResponseWithParams;
-  const { search, company, category, shipping, price } = params;
+  const { search, company, category, shipping, order, price } = params;
   return (
     <Form className="border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/* search */}
@@ -15,6 +16,28 @@ export default function Filters() {
         name="search"
         defaultValue={search}
       />
+      {/* category */}
+      <FormSelect
+        label="select category"
+        name="category"
+        options={meta.categories}
+        defaultValue={category}
+      />
+      {/* companies */}
+      <FormSelect
+        label="select company"
+        name="company"
+        options={meta.companies}
+        defaultValue={company}
+      />
+      {/* order */}
+      <FormSelect
+        label="order by"
+        name="order"
+        options={["a-z", "z-a", "high", "low"]}
+        defaultValue={order}
+      />
+
       <Button type="submit" size="sm" className="self-end mb-2">
         Search
       </Button>
