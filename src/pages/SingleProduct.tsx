ad15000ef5200097d0efcfee691b6e1ff58bc791
Aman11b <amansingh1100b@gmail.com>
2026-06-26 00:54:03 +0530

    single page render image and info

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -12,13 +12,65 @@ import { Separator } from "@/components/ui/separator";
 import { type LoaderFunction } from "react-router-dom";
 
 import { SelectProductAmount, SelectProductColor } from "../components";
+import { Slice } from "lucide-react";
 
-export const loader: LoaderFunction = async ({ params }) => {
-  console.log(params);
-  return null;
+export const loader: LoaderFunction = async ({
+  params,
+}): Promise<SingleProductResponse> => {
+  const response = await customFetch<SingleProductResponse>(
+    `/products/${params.id}`,
+  );
+  return { ...response.data };
 };
 
 function SingleProduct() {
-  return <h1 className="text-4xl">AbouSingleProduct Page</h1>;
+  const { data: product } = useLoaderData() as SingleProductResponse;
+  const { image, title, price, description, colors, company } =
+    product.attributes;
+  const dollersAmount = formatAsDollars(price);
+  const [productColor, setProductColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+  const addToCart = () => {
+    console.log("add to cart");
+  };
+  return (
+    <section>
+      <div className="flex gap-x-2 h-6 items-center">
+        <Button asChild varient="link" size="sm">
+          <Link to="/">Home</Link>
+        </Button>
+        <Separator orientation="vertical" />
+        <Button asChild varient="link" size="sm">
+          <Link to="/products">Products</Link>
+        </Button>
+      </div>
+      {/* products */}
+      <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-26">
+        {/* image */}
+        <img
+          src={image}
+          alt={title}
+          className="w-96 h-96 object-cover rounded-lg lg:w-full"
+        />
+        {/* product info*/}
+        <div>
+          <h1 className="capitalize text-3xl font-bold">{title}</h1>
+          <h4 className="text-xl mt-2">{company}</h4>
+          <p className="mt-3 text-md bg-muted inline-block p-2 rounded-md">
+            {dollersAmount}
+          </p>
+          <p className="mt-6 leading-8">{description}</p>
+          {/* colors */}
+          <SelectProductColor />
+          {/* amuont */}
+          <SelectProductAmount />
+          {/* cart byutton */}
+          <Button size="lg" className="mt-10" onClick={addToCart}>
+            Add to cart
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
 }
 export default SingleProduct;
