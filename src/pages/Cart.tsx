11f6efd66f4c1c2a6021ab7da76fdd08e1fc3482
Aman11b <amansingh1100b@gmail.com>
2026-06-26 20:49:38 +0530

    cart total component

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -5,6 +5,7 @@ import { Button } from "../components/ui/button";
 import { SectionTitle } from "../components";
 import CartItemsList from "../components/CartItemsList";
 import CartTotal from "../components/CartTotal";
+import { Separator } from "../components/ui/separator";
 
 function Cart() {
   // temp
