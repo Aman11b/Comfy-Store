1c54052ff0283984af364f98c57e85021c57a2e3
Aman11b <amansingh1100b@gmail.com>
2026-06-27 23:13:19 +0530

    order list component

--- src/components/OrdersList.tsx
+++ src/components/OrdersList.tsx
@@ -1,3 +1,49 @@
+import { useLoaderData } from "react-router-dom";
+
+import { type OrdersResponse } from "../utils";
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../components/ui/table";
+
 export default function OrdersList() {
-  return <div>OrdersList</div>;
+  const { data: order, meta } = useLoaderData() as OrdersResponse;
+  return (
+    <div className="mt-16">
+      <h4 className="mb-4 capitalize">Total Orders: {meta.pagination.total}</h4>
+      <Table>
+        <TableCaption>A list of your recent orders.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Address</TableHead>
+            <TableHead className="w-[100px]">Products</TableHead>
+            <TableHead className="w-[100px]">Cost</TableHead>
+            <TableHead>Date</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {order.map((order) => {
+            const { name, address, numItemsInCart, orderTotal, createdAt } =
+              order.attributes;
+            return (
+              <TableRow key={order.id}>
+                <TableCell>{name}</TableCell>
+                <TableCell>{address}</TableCell>
+                <TableCell className="text-center">{numItemsInCart}</TableCell>
+                <TableCell>{orderTotal}</TableCell>
+                <TableCell>{new Date(createdAt).toDateString()}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,8 +1,14 @@
+/* eslint-disable react-refresh/only-export-components */
 import { type LoaderFunction, redirect, useLoaderData } from "react-router-dom";
 
 import { toast } from "sonner";
 import { type ReduxStore } from "../store";
 import { customFetch, type OrdersResponse } from "../utils";
+import {
+  ComplexPaginationContainer,
+  OrdersList,
+  SectionTitle,
+} from "../components";
 
 export const loader =
   (store: ReduxStore): LoaderFunction =>
@@ -31,8 +37,17 @@ export const loader =
   };
 
 function Orders() {
-  const { meta, data } = useLoaderData() as OrdersResponse;
-  console.log(data);
-  return <h1 className="text-4xl">Orders</h1>;
+  const { meta } = useLoaderData() as OrdersResponse;
+  if (meta.pagination.total < 1) {
+    return <SectionTitle text="Please make an order" />;
+  }
+
+  return (
+    <>
+      <SectionTitle text="Your Order" />
+      <OrdersList />
+      <ComplexPaginationContainer />
+    </>
+  );
 }
 export default Orders;
